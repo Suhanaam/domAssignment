c5c91bac82a307816dd86c68f6e1d861e81bedbc
suhana <suhanapks@gmail.com>
2024-08-11 20:32:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeimage()
+{
+    let srkimage=document.getElementById("srk")
+    srkimage.style.border="10px solid green"
+    srkimage.setAttribute("src","DOMIMAGES/srk2.jpg")
+}
+function removechange()
+{
+    let srkimage=document.getElementById("srk")
+    srkimage.setAttribute("src","DOMIMAGES/srk1.jpg")
+}
